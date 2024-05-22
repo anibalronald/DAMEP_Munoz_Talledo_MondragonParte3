2c6d51bf20677b7aec45a0cf24b389f8ad25db8e
Alberto Mondragon <ismaael20_mondragon@hotmail.com>
2024-05-22 11:00:08 -0500

    Parte 3 Mondragon

--- app/src/main/java/com/example/damep_munoz_talledo_mondragon/MainActivity.java
+++ app/src/main/java/com/example/damep_munoz_talledo_mondragon/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.damep_munoz_talledo_mondragon;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button btnEnviar = findViewById(R.id.btnEnviar);
+        btnEnviar.setOnClickListener(view -> Toast.makeText(this, "Se envio sus datos", Toast.LENGTH_SHORT).show());
+
     }
 }
